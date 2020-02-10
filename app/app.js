ef0a845ed5178d985f5e034a091f1f50e6325f03
Nsuliman <nsuliman88@gmail.com>
2020-02-10 16:21:07 +0200

    fs completed

--- app/app.js
+++ app/app.js
@@ -6,6 +6,9 @@ const util = require('util');
 
 const client = new net.Socket();
 
+const reader = ('./lib/reader.js');
+
+/***************************************** Read / Write File System **************************************************/
 // FS File( read , write )
 let file = `${__dirname}/../files/data.text`;
 
@@ -16,8 +19,8 @@ let readFilepromisify = util.promisify(fs.readFile);
 readFilepromisify(file)
     .then(data => 
         {
-            console.log('data promisify : ',JSON.parse(data.toString()))
-            return JSON.parse(data.toString());
+            console.log('data promisify : ', data.toString());
+            return data.toString();
         })
     .then(data => writeFile( file, data))
     .catch(error => console.error('There is an error , promise',error))
@@ -37,12 +40,13 @@ async function readFileAsync(file) {
 
 readFileAsync(file);
 
+let writeFilepromisify = util.promisify(fs.writeFile);
+
 const writeFile = (file,data) =>
 {
+    data = data.toUpperCase();
   console.log('data in write function : ', data);
-//   data.firstName = 'Nawal  Suliman ';
-//   console.log('data after updated : ', data);
   let data2 = JSON.stringify(data);
-  writeFilepromisify(file,data2);
+  writeFilepromisify(file,data);
   return data2;
 };
